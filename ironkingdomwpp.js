c0ab7d5a64bc5f5e039d38c25c34f4f68bad2bf5
Raffozo <39443512+Rafaellegend@users.noreply.github.com>
2022-10-23 17:59:00 -0300

    Add files via upload

---
+++ ironkingdomwpp.js
@@ -0,0 +1,13 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['arcane'] = 'Arcane';
+  	CONFIG.DND5E.weaponProperties['area'] = 'Effect Area';
+	CONFIG.DND5E.weaponProperties['granade'] = 'Granade';
+	CONFIG.DND5E.weaponProperties['hail'] = 'Hail';	
+	CONFIG.DND5E.weaponProperties['hidden'] = 'Hidden';
+	CONFIG.DND5E.weaponProperties['missshot'] = 'Miss Shot';
+	CONFIG.DND5E.weaponProperties['magazine'] = 'Magazine';
+	CONFIG.DND5E.weaponProperties['rearm'] = 'Rearm';
+	CONFIG.DND5E.weaponProperties['stormy'] = 'Stormy';
+	CONFIG.DND5E.weaponProperties['stormygenerator'] = 'Stormy Generator';
+	
+});
